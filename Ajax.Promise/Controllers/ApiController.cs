06f944baf585738a4ad0fc99afc1316f5e8040c5
fuen31-wf <fuen31wf@gmail.com>
2024-01-23 09:04:38 +0800

    加入專案檔案。

---
+++ Ajax.Promise/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Ajax.Promise.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ajax.Promise.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
---
+++ Ajax.Promise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ajax.Promise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
